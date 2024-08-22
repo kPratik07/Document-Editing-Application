aea52951b457a03db95f8d0d554b9d9056ab8f89
PRATIK RAJ <kpratik071997@gmail.com>
2024-08-22 14:47:17 +0530

    code added for the app

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Adjust to your back end URL
+  baseURL: "http://localhost:5000/api",
 });
 
 export const fetchDocuments = () => api.get("/documents");
